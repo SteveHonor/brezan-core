713d0d311e734cdc1fd65c26239e5e53b0b910b1
Steve J <steve@honors.com.br>
2019-03-26 01:54:16 -0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins '*'
+     origins 'http://sistema.brezanfotografia.com.br.s3-website-us-east-1.amazonaws.com'
 
      resource '*',
        headers: :any,
